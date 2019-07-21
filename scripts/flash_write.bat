9956407a7f967c0dc45af8a735266adf5c37820f
wuxx <wujianhua0924@gmail.com>
2019-07-21 21:43:51 +0800

    update

--- scripts/flash_write.bat
+++ scripts/flash_write.bat
@@ -1,1 +1,2 @@
 ..\OpenOCD-20190426-0.10.0\bin\openocd.exe -f ..\OpenOCD-20190426-0.10.0\tcl\target\stm32f1x_cmsisdap.cfg -c "init;halt;reset halt;flash write_image erase flash_image.bin 0x08000000;shutdown;"
+pause
