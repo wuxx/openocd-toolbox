7350f154dae636f5e1cf3472c1969f9c1b738909
wuxx <wujianhua0924@gmail.com>
2020-02-04 19:44:40 +0800

    update

---
+++ scripts/windows/efm32/flash_dump.bat
@@ -0,0 +1,5 @@
+..\..\OpenOCD-20190426-0.10.0\bin\openocd.exe ^
+    -f ..\..\OpenOCD-20190426-0.10.0\tcl\target\efm32_cmsisdap.cfg ^
+    -c "init;halt;reset halt;dump_image flash_image_dump.bin 0x08000000 0x20000;shutdown;"
+pause
+
---
+++ scripts/windows/efm32/flash_write.bat
@@ -0,0 +1,6 @@
+::若烧录hex文件，请将 "flash_image.bin 0x08000000" 改成 "flash_image.hex"
+
+..\..\OpenOCD-20190426-0.10.0\bin\openocd.exe ^
+    -f ..\..\OpenOCD-20190426-0.10.0\tcl\target\efm32_cmsisdap.cfg ^
+    -c "init;halt;reset halt;flash write_image erase flash_image.bin 0x08000000;shutdown;"
+pause
