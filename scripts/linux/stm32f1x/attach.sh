964fd720d9a683a245af6348dd1c3ecb428078fd
wuxx <wujianhua0924@gmail.com>
2020-02-04 19:30:21 +0800

    add linux platform support

---
+++ scripts/linux/stm32f1x/attach.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo openocd -f ../tcl/target/stm32f1x_cmsisdap.cfg
---
+++ scripts/linux/stm32f1x/flash_erase.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#xxx.hex or xxx.bin
+
+#stm32f103c8t6
+FLASH_ADDR=0x08000000
+FLASH_SIZE=0x00010000 #128KB
+
+CURRENT_DIR=$(cd $(dirname $0); pwd)
+
+CONFIG=${CURRENT_DIR}/../tcl/target/stm32f1x_bcm2835gpio_swd.cfg
+
+sudo openocd -s ${CURRENT_DIR}/tcl -f ${CONFIG} -c \
+"   init;
+    halt;
+    reset halt;
+    flash erase_address ${FLASH_ADDR} ${FLASH_SIZE};
+    shutdown;
+"
