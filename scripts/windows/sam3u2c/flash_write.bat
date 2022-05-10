ab0ae7d6cc830a01e8dbbf199f9d11a7772eb3c2
wuxx <wujianhua0924@gmail.com>
2022-05-10 16:44:05 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	renamed:    ../../../OpenOCD-20190426-0.10.0/tcl/target/sam3u2c_cmsis_dap.cfg -> ../../../OpenOCD-20190426-0.10.0/tcl/target/sam3u2c_cmsisdap.cfg
    	modified:   flash_write.bat

--- scripts/windows/sam3u2c/flash_write.bat
+++ scripts/windows/sam3u2c/flash_write.bat
@@ -1,6 +1,6 @@
-::若烧录hex文件，请将 "flash_image.bin 0x08000000" 改成 "flash_image.hex"
+::若烧录hex文件，请将 "flash_image.bin 0x00080000" 改成 "flash_image.hex"
 
 ..\..\..\OpenOCD-20190426-0.10.0\bin\openocd.exe ^
     -f ..\..\..\OpenOCD-20190426-0.10.0\tcl\target\sam3u2c_cmsisdap.cfg ^
-    -c "init;halt;reset halt;flash write_image erase flash_image.bin 0x08000000;shutdown;"
+    -c "init;halt;reset halt;flash write_image erase flash_image.bin 0x00080000;shutdown;"
 pause
